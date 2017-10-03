c286d1caf4bdcf8d55fd2ac88cf010491affe33e
nach9 <nur.achdiansyah@gmail.com>
2017-10-04 00:54:12 +0700

    fix minor err

--- models/groups.js
+++ models/groups.js
@@ -5,49 +5,52 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('data.db');
 
 class Groups {
-  constructor (data){
+  constructor(data) {
     this.id = data.id
     this.name_of_group = data.name_of_group
 
   }
 
-  static selectAll(cb){
-    db.all('SELECT * FROM groups', (err,rows)=>{
-      let dataRows=[]
-      rows.forEach(row=>{
+  static selectAll(cb) {
+    db.all('SELECT * FROM groups', (err, rows) => {
+      let dataRows = []
+      rows.forEach(row => {
         let groups = new Groups(row)
         dataRows.push(groups)
       })
-        cb(dataRows)
+      cb(dataRows)
     })
   }
 
-  static insertNew (name_of_group ,cb){
-    db.run(`insert into groups values( null , '${name_of_group}')` , (err)=> {
+  static insertNew(name_of_group, cb) {
+    db.run(`insert into Groups values( null , '${name_of_group}')`, (err) => {
       cb(err, this.lastID)
     })
   }
 
-  static selectBy ( column , value , cb){
-    db.all(`select * from groups where ${column} = '${value}'`,(err,rows)=>{
+
+  static selectBy(column, value, cb) {
+    db.all(`select * from groups where ${column} = '${value}'`, (err, rows) => {
       let dataGroups = []
-      rows.forEach((row)=>{
+      rows.forEach((row) => {
         let groups = new Groups(row)
         this.id = groups.id
+        console.log(dataGroups);
         dataGroups.push(groups)
       })
+      console.log(dataGroups);
       cb(dataGroups)
     })
   }
 
-  static editID (  name_of_group, cb ){
-    db.run(`update groups set name_of_group='${name_of_group}'  where id = '${this.id}'`,err=>{
+  static editID(name_of_group, cb) {
+    db.run(`update groups set name_of_group='${name_of_group}'  where id = '${this.id}'`, err => {
       cb()
     })
   }
 
-  static deleteID (cb){
-    db.run(`delete from groups where id = '${this.id}'`,err=>{
+  static deleteID(cb) {
+    db.run(`delete from groups where id = '${this.id}'`, err => {
       cb()
     })
   }
--- routes/groups.js
+++ routes/groups.js
@@ -7,82 +7,47 @@ const Groups = require('../models/groups')
 
 
 // groups
-router.get('/',  (req, res)=> {
-  Groups.selectAll(rows=>{
-    res.render('groups' , { dataGroups:rows} )
+router.get('/', (req, res) => {
+  Groups.selectAll(rows => {
+    res.render('groups', {
+      dataGroups: rows
+    })
   })
-  // db.all('SELECT * FROM Groups', (err,rows)=> {
-  //   res.render('groups' , { dataGroups:rows} )
-  // })
 })
 
-router.post('/',  (req,res)=> {
-  Groups.insertNew( ( err,id )=>{
+router.post('/', (req, res) => {
+  Groups.insertNew(req.body.name_of_group, (err, id) => {
     res.redirect('groups')
   })
-  db.all(`insert into Groups values( null , '${req.body.name_of_group}'   )` , function(err){
-  //   if(err){
-  //     console.log(err);
-  //   } else{
-  //     res.redirect('groups')
-  //   }
-  // })
 })
 
-router.get('/edit/:id' , (req,res)=>{
-  Groups.selectBy('id' , req.params.id , rows=>{
-    res.render('groups_edit', { dataGroups : rows })
-  } )
-  // db.all(`select * from groups where id = '${req.params.id}'`,(err,rows)=>{
-  //   if(err){
-  //     console.log(err);
-  //   }
-  //   else {
-  //     res.render('groups_edit', { dataGroups : rows })
-  //   }
-  // })
+router.get('/edit/:id', (req, res) => {
+  Groups.selectBy('id', req.params.id, (rows) => {
+    res.render('groups_edit', {
+      dataGroups: rows
+    })
+  })
 })
 
 
-router.post('/edit/:id' , (req , res) =>{
-  Groups.editID( req.body.name_of_group , ()=>{
+router.post('/edit/:id', (req, res) => {
+  Groups.editID(req.body.name_of_group, () => {
     res.redirect('../../groups')
   })
-  // db.all(`update groups set name_of_group='${req.body.name_of_group}' where id='${req.params.id}'`,(err,rows)=>{
-  //   if(err){
-  //     console.log(err);
-  //   } else{
-  //     res.redirect('../../groups')
-  //   }
-  // })
 })
 
-router.get('/delete/:id' , (req,res) => {
-  Groups.selectBy('id',req.params.id, rows=>{
-    res.render('groups_delete' , { dataGroups : rows })
-  })
-  // db.all(`select * from groups where id='${req.params.id}'`, (err,rows)=>{
-  //   if(err){
-  //     console.log(err);
-  //   }
-  //   else {
-  //     res.render('groups_delete' , { dataGroups : rows })
-  //   }
+router.get('/delete/:id', (req, res) => {
+  Groups.selectBy('id', req.params.id, rows => {
+    res.render('groups_delete', {
+      dataGroups: rows
+    })
   })
 })
 
-router.post('/delete/:id', (req,res)=>{
-  Groups.deleteID( ()=>{
+router.post('/delete/:id', (req, res) => {
+  Groups.deleteID(() => {
     res.redirect('../../groups')
   })
-  // db.all(`delete from groups where id='${req.params.id}'`, (err,rows)=>{
-  //   if(err){
-  //     console.log(err);
-  //   }
-  //   else{
-  //     res.redirect('../../groups')
-  //   }
-  // })
 })
 
 module.exports = router;
